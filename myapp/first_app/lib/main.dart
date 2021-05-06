9d3c343b6087e1a72f26a64301e3c82ffe71305d
SuyeshBadge <suyeshbadge@gmail.com>
2021-05-06 16:28:04 +0530

    Flutter

--- myapp/first_app/lib/main.dart
+++ myapp/first_app/lib/main.dart
@@ -1,41 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/pages/home.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: HomePage(),
-    theme: ThemeData(
-        scaffoldBackgroundColor: Colors.lightBlue, primarySwatch: Colors.blue),
-  ));
+  runApp(MyApp());
 }
 
-class HomePage extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              colors: [
-                Colors.blue,
-                Colors.red,
-              ],
-            ),
-          ),
-          child: Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(
-                fontSize: 48.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ),
-      ),
+    return MaterialApp(
+      home: Homepage(),
     );
   }
 }
---
+++ myapp/first_app/lib/pages/home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Colors.blue,
+                Colors.red,
+              ],
+            ),
+          ),
+          child: Center(
+            child: Text(
+              'Hello, World!',
+              style: TextStyle(
+                fontSize: 48.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
